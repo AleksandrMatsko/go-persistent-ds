978e8aa5b7ed913d462f772eda31bbe1a8f02ff7
AleksandrMatsko <AMatsko54@gmail.com>
2024-12-04 01:18:17 +0700

    tests: for other methods

--- map_test.go
+++ map_test.go
@@ -3,10 +3,11 @@ package main
 import (
 	"errors"
 	"go-persistent-ds/internal"
+	"maps"
 	"testing"
 )
 
-func versionOk(t *testing.T, got, expected uint64) {
+func versionShouldBe(t *testing.T, got, expected uint64) {
 	if got != expected {
 		t.Errorf("expected version: %v, got: %v", expected, got)
 	}
@@ -30,7 +31,34 @@ func errShouldBe(t *testing.T, gotErr error, expectedErr error) {
 	}
 }
 
-func TestMap_GetSet(t *testing.T) {
+func getBranchedMap(t *testing.T) *Map[string, string] {
+	m, initialVersion := NewMap[string, string]()
+	versionShouldBe(t, initialVersion, 0)
+
+	v, err := m.Set(0, "a", "0")
+	errIsNil(t, err)
+	versionShouldBe(t, v, 1)
+
+	v, err = m.Set(1, "b", "1")
+	errIsNil(t, err)
+	versionShouldBe(t, v, 2)
+
+	v, err = m.Set(1, "c", "1")
+	errIsNil(t, err)
+	versionShouldBe(t, v, 3)
+
+	v, err = m.Set(2, "c", "2")
+	errIsNil(t, err)
+	versionShouldBe(t, v, 4)
+
+	v, err = m.Set(3, "b", "2")
+	errIsNil(t, err)
+	versionShouldBe(t, v, 5)
+
+	return m
+}
+
+func TestMap_with_GetSet(t *testing.T) {
 	t.Run("Set and Get works expected", func(t *testing.T) {
 		t.Parallel()
 
@@ -38,7 +66,7 @@ func TestMap_GetSet(t *testing.T) {
 		val := "a"
 
 		m, initialVersion := NewMap[string, string]()
-		versionOk(t, initialVersion, 0)
+		versionShouldBe(t, initialVersion, 0)
 
 		gotVal, found := m.Get(initialVersion, key)
 		isTrue(t, !found)
@@ -46,7 +74,7 @@ func TestMap_GetSet(t *testing.T) {
 
 		gotVersion, err := m.Set(initialVersion, key, val)
 		errIsNil(t, err)
-		versionOk(t, gotVersion, 1)
+		versionShouldBe(t, gotVersion, 1)
 
 		gotVal, found = m.Get(initialVersion, key)
 		isTrue(t, !found)
@@ -57,22 +85,45 @@ func TestMap_GetSet(t *testing.T) {
 		isTrue(t, gotVal == val)
 	})
 
+	t.Run("Set updates existed key", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, err = m.Set(1, "a", "2")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 2)
+
+		val, found := m.Get(1, "a")
+		isTrue(t, found)
+		isTrue(t, val == "1")
+
+		val, found = m.Get(2, "a")
+		isTrue(t, found)
+		isTrue(t, val == "2")
+	})
+
 	t.Run("Set for not existing version", func(t *testing.T) {
 		t.Parallel()
 
 		m, initialVersion := NewMap[string, string]()
-		versionOk(t, initialVersion, 0)
+		versionShouldBe(t, initialVersion, 0)
 
 		newVersion, err := m.Set(2, "a", "a")
 		errShouldBe(t, err, internal.ErrVersionNotFound)
-		versionOk(t, newVersion, 0)
+		versionShouldBe(t, newVersion, 0)
 	})
 
 	t.Run("Get for not existing version", func(t *testing.T) {
 		t.Parallel()
 
 		m, initialVersion := NewMap[string, string]()
-		versionOk(t, initialVersion, 0)
+		versionShouldBe(t, initialVersion, 0)
 
 		_, err := m.Set(0, "a", "1")
 		errIsNil(t, err)
@@ -85,28 +136,7 @@ func TestMap_GetSet(t *testing.T) {
 	t.Run("Set and Get with different branches", func(t *testing.T) {
 		t.Parallel()
 
-		m, initialVersion := NewMap[string, string]()
-		versionOk(t, initialVersion, 0)
-
-		v, err := m.Set(0, "a", "0")
-		errIsNil(t, err)
-		versionOk(t, v, 1)
-
-		v, err = m.Set(1, "b", "1")
-		errIsNil(t, err)
-		versionOk(t, v, 2)
-
-		v, err = m.Set(1, "c", "1")
-		errIsNil(t, err)
-		versionOk(t, v, 3)
-
-		v, err = m.Set(2, "c", "2")
-		errIsNil(t, err)
-		versionOk(t, v, 4)
-
-		v, err = m.Set(3, "b", "2")
-		errIsNil(t, err)
-		versionOk(t, v, 5)
+		m := getBranchedMap(t)
 
 		type testCase struct {
 			givenVersion  uint64
@@ -124,9 +154,9 @@ func TestMap_GetSet(t *testing.T) {
 				foundShouldBe: false,
 			})
 
-		for v = range 5 {
+		for v := range 5 {
 			testCases = append(testCases, testCase{
-				givenVersion:  v + 1,
+				givenVersion:  uint64(v + 1),
 				givenKey:      "a",
 				valShouldBe:   "0",
 				foundShouldBe: true,
@@ -220,6 +250,357 @@ func TestMap_GetSet(t *testing.T) {
 			gotVal, found := m.Get(singleCase.givenVersion, singleCase.givenKey)
 			isTrue(t, found == singleCase.foundShouldBe)
 			isTrue(t, gotVal == singleCase.valShouldBe)
+		}
+	})
+}
+
+func TestMap_with_GetSetDelete(t *testing.T) {
+	t.Run("Get after Delete", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, ok := m.Delete(1, "a")
+		isTrue(t, ok)
+		versionShouldBe(t, v, 2)
+
+		val, found := m.Get(2, "a")
+		isTrue(t, !found)
+		isTrue(t, val == "")
+	})
+
+	t.Run("Delete not existing element", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, ok := m.Delete(1, "b")
+		isTrue(t, !ok)
+		versionShouldBe(t, v, 0)
+	})
+
+	t.Run("Delete with not existing version", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, ok := m.Delete(2, "a")
+		isTrue(t, !ok)
+		versionShouldBe(t, v, 0)
+	})
+}
+
+func TestMap_Len(t *testing.T) {
+	t.Run("With empty map", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		size, err := m.Len(0)
+		errIsNil(t, err)
+		isTrue(t, size == 0)
+	})
+
+	t.Run("With not existing version", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		size, err := m.Len(2)
+		errShouldBe(t, err, internal.ErrVersionNotFound)
+		isTrue(t, size == 0)
+	})
+
+	t.Run("After Set", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		size, err := m.Len(1)
+		errIsNil(t, err)
+		isTrue(t, size == 1)
+	})
+
+	t.Run("After Set and then update key", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, err = m.Set(1, "a", "2")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 2)
+
+		size, err := m.Len(1)
+		errIsNil(t, err)
+		isTrue(t, size == 1)
+
+		size, err = m.Len(2)
+		errIsNil(t, err)
+		isTrue(t, size == 1)
+	})
+
+	t.Run("After Set and then Delete", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, ok := m.Delete(1, "a")
+		isTrue(t, ok)
+		versionShouldBe(t, v, 2)
+
+		size, err := m.Len(1)
+		errIsNil(t, err)
+		isTrue(t, size == 1)
+
+		size, err = m.Len(2)
+		errIsNil(t, err)
+		isTrue(t, size == 0)
+	})
+
+	t.Run("With version branches", func(t *testing.T) {
+		t.Parallel()
+
+		m := getBranchedMap(t)
+
+		type testCase struct {
+			givenVersion uint64
+			expectedLen  int
+		}
+
+		testCases := []testCase{
+			{
+				givenVersion: 0,
+				expectedLen:  0,
+			},
+			{
+				givenVersion: 1,
+				expectedLen:  1,
+			},
+			{
+				givenVersion: 2,
+				expectedLen:  2,
+			},
+			{
+				givenVersion: 3,
+				expectedLen:  2,
+			},
+			{
+				givenVersion: 4,
+				expectedLen:  3,
+			},
+			{
+				givenVersion: 5,
+				expectedLen:  3,
+			},
+		}
+
+		for i, c := range testCases {
+			t.Logf("case %v: Len(%v) -> %v", i+1, c.givenVersion, c.expectedLen)
+			gotLen, err := m.Len(c.givenVersion)
+			errIsNil(t, err)
+			isTrue(t, gotLen == c.expectedLen)
+		}
+	})
+}
+
+func TestMap_ToGoMap(t *testing.T) {
+	t.Run("With empty map", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		gotMap, err := m.ToGoMap(0)
+		errIsNil(t, err)
+		isTrue(t, maps.Equal(gotMap, map[string]string{}))
+	})
+
+	t.Run("With not existing version", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		gotMap, err := m.ToGoMap(2)
+		errShouldBe(t, err, internal.ErrVersionNotFound)
+		isTrue(t, gotMap == nil)
+	})
+
+	t.Run("After Set", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		gotMap, err := m.ToGoMap(1)
+		errIsNil(t, err)
+		isTrue(t, maps.Equal(
+			gotMap,
+			map[string]string{
+				"a": "1",
+			}))
+	})
+
+	t.Run("After Set and then update key", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, err = m.Set(1, "a", "2")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 2)
+
+		gotMap, err := m.ToGoMap(1)
+		errIsNil(t, err)
+		isTrue(t, maps.Equal(
+			gotMap,
+			map[string]string{
+				"a": "1",
+			}))
+
+		gotMap, err = m.ToGoMap(2)
+		errIsNil(t, err)
+		isTrue(t, maps.Equal(
+			gotMap,
+			map[string]string{
+				"a": "2",
+			}))
+	})
+
+	t.Run("After Set and then Delete", func(t *testing.T) {
+		t.Parallel()
+
+		m, initialVersion := NewMap[string, string]()
+		versionShouldBe(t, initialVersion, 0)
+
+		v, err := m.Set(0, "a", "1")
+		errIsNil(t, err)
+		versionShouldBe(t, v, 1)
+
+		v, ok := m.Delete(1, "a")
+		isTrue(t, ok)
+		versionShouldBe(t, v, 2)
+
+		gotMap, err := m.ToGoMap(1)
+		errIsNil(t, err)
+		isTrue(t, maps.Equal(
+			gotMap,
+			map[string]string{
+				"a": "1",
+			}))
+
+		gotMap, err = m.ToGoMap(2)
+		errIsNil(t, err)
+		isTrue(t, maps.Equal(gotMap, map[string]string{}))
+	})
+
+	t.Run("With version branches", func(t *testing.T) {
+		t.Parallel()
+
+		m := getBranchedMap(t)
+
+		type testCase struct {
+			givenVersion uint64
+			expectedMap  map[string]string
+		}
+
+		testCases := []testCase{
+			{
+				givenVersion: 0,
+				expectedMap:  map[string]string{},
+			},
+			{
+				givenVersion: 1,
+				expectedMap: map[string]string{
+					"a": "0",
+				},
+			},
+			{
+				givenVersion: 2,
+				expectedMap: map[string]string{
+					"a": "0",
+					"b": "1",
+				},
+			},
+			{
+				givenVersion: 3,
+				expectedMap: map[string]string{
+					"a": "0",
+					"c": "1",
+				},
+			},
+			{
+				givenVersion: 4,
+				expectedMap: map[string]string{
+					"a": "0",
+					"b": "1",
+					"c": "2",
+				},
+			},
+			{
+				givenVersion: 5,
+				expectedMap: map[string]string{
+					"a": "0",
+					"b": "2",
+					"c": "1",
+				},
+			},
+		}
+
+		for i, c := range testCases {
+			t.Logf("case %v: version %v", i+1, c.givenVersion)
+			gotMap, err := m.ToGoMap(c.givenVersion)
+			errIsNil(t, err)
+			isTrue(t, maps.Equal(gotMap, c.expectedMap))
 		}
 	})
 }
